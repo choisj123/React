7ad86056bba7da92ba22ff5eaa60abcb1111fe7d
Sujin Choi(최수진 - Q-Service Dev. Team) <98chltnwls@gmail.com>
2023-10-31 18:28:49 +0900

    ch.03 create-react-app

--- MyButton.js
+++ MyButton.js
@@ -9,3 +9,42 @@ function MyButton(props){
 }
 const domContainer = document.querySelector('#root');
 ReactDom.render(React.createElement(MyButton), domContainer);
+
+
+
+// JSX를 사용한 코드
+class Hello extends React.Component{
+  render(){
+    return <div>Hello {this.props.toWhat}</div>
+  }
+}
+ReactDom.render(
+  <Hello toWhat="world"/>,
+  document.getElementById('root')
+)
+
+// JSX를 사용하지 않은 코드
+class Hello extends React.Component{
+  render(){
+    return React.createElement('div', null, 'Hello ${this.props.toWhat}');
+  }
+}
+ReactDom.render(
+  React.createElement(Hello, {toWhat: 'World'}, null),
+  document.getElementById('root')
+)
+
+// JSX를 사용한 코드 
+const element1 = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>
+)
+
+// JSX를 사용하지 않은 코드
+const element2 = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
+)
+
---
+++ react-app/src/chapter_03/Book.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Book(props){
+  return(
+    <div>
+      <h1>{`이 책의 이름은 ${props.name}입니다.`}</h1>
+      <h2>{`이 책은 총 ${props.numOfPage}페이지로 이뤄져 있습니다.`}</h2>
+    </div>
+  );
+}
+
+// JSX를 사용하지 않은 코드
+function Book2(props){
+  return React.createElement(
+    'div',
+    null,
+    [
+      React.createElement(
+        'h1',
+        null,
+        `이 책의 이름은 ${props.name}입니다.`
+      ),
+      React.createElement(
+        'h2',
+        null,
+        `이 책은 총 ${props.numOfPage}페이지로 이뤄져 있습니다.`
+      )
+    ]
+  );
+}
+
+export default Book;
